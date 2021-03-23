5545fe4b0a5904f1518d4fab39ce92f6b1f3fb9d
leandrososa <52506861+leandrososa@users.noreply.github.com>
2021-03-22 23:29:51 -0300

    schema setup

--- server.js
+++ server.js
@@ -2,13 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+const { Schema } = mongoose;
+
+const shortenUrlSchema = new Schema({
+  originalUrl:  String
+});
+
+let ShortenURL = mongoose.model('ShortenURL', shortenUrlSchema);
+
+const createShortenURL = (url, done) => {
+  let newURL = new ShortenURL({originalUrl: url});
+
+  newURL.save(function(err, data){
+    if (err) return console.error(err);
+    done(null, data)
+  });
+}
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -17,6 +40,10 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl/new', (req,res) => {
+
 });
 
 app.listen(port, function() {
